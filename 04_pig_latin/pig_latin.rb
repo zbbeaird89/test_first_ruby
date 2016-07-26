dc170696d48012ad040e81978724ec73c22cbf21
Zach Beaird <zbbeaird1989@gmail.com>
2016-07-25 21:31:43 -0500

    translate 2 constonants

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -3,12 +3,15 @@ def translate(word)
 	letters = word.split("")
 	vowels  = ["a", "e", "i", "u", "o"]
 
-	for vowel in vowels
-		if vowel == letters[0]
-			letters.push("ay")
-		end
+	if vowels.include?(letters[0])
+		letters.push("ay")
+	else 
+		letters.insert(letters.length, letters.delete_at(0))
+		letters.push("ay")
 	end
 
 	return letters.join("")
 end
+
+
 
