3f724181f7bc9071c2dddeb8797d739b6b553cac
Zach Beaird <zbbeaird1989@gmail.com>
2016-08-02 22:01:38 -0500

    Starting09_timer

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,6 +1,44 @@
 class Book
 
 	def title=(name)
+		small_words = %w[and by is in the of a an]
+		words = name.split(" ")
+		new_words = []
+		
+		words.each_with_index do |word, i| 
+			if small_words.include?(word)
+				if i == 0
+					new_words << word.capitalize
+				else
+					new_words << word
+				end
+			else
+				new_words << word.capitalize
+			end
+		end
+
+		@name = new_words.join(" ")
+	end
+
+	def title 
+		@name
+	end
+
+end
+
+book = Book.new
+
+book.title = "war and peace"
+
+puts book.title
+
+
+
+
+=begin
+class Book
+
+	def title=(name)
 		words = name.split(" ")
 		new_words = []
 		
@@ -21,3 +59,4 @@ book.title = "stewart little"
 
 puts book.title
 
+=end
